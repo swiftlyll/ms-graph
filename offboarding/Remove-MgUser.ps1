b59ff71a67a55d37be6e108c7578289f708f72e4
swiftlyll <125161054+swiftlyll@users.noreply.github.com>
2025-06-03 13:25:08 -0700

    created offboarding dir

---
+++ offboarding/Remove-MgUser.ps1
@@ -0,0 +1,16 @@
+# should have:
+#     user
+#         hidden from gal 
+#         enabled
+#         email 
+#         name 
+#         id 
+#     member_of
+#         id 
+#         name
+#     licenses
+#         sku
+#         formal name 
+#     onedrive storage used 
+#     mailbox storage used
+#         is archiving in place?
